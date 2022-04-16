6d93ef873bf4bb7a380ba7ed94d18b5f66005b91
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2022-04-16 15:25:30 +0530

    second commit

--- song/views.py
+++ song/views.py
@@ -1,7 +1,13 @@
+from turtle import down
 from django.shortcuts import render
 import pafy
 import youtube_dl
+import os 
 from .forms import songForm
+from django.shortcuts import redirect
+from django.contrib import messages
+
+os.chdir(r'C:\Users\Venom\Desktop\django\youtube download songs')
 # Create your views here.
 
 def downloadsong(response):
@@ -10,13 +16,29 @@ def downloadsong(response):
         form = songForm(response.POST)
         if form.is_valid():
             yurl = form.cleaned_data['songurl']
-            url = yurl
-            video = pafy.new(url)
-            bestaudio = video.getbestaudio()
-            bestaudio.download()
-            print("download complete")
+            if executedownload(yurl):
+                print("Song Downloaded") 
+                messages.success(response, 'Song Downloaded Successfully')
+                redirect("downloadsong")    
+            else:
+                print("Please Try again")
+                redirect("downloadsong")    
             form.save()
             print("Form Data saved")
+            
     else: 
         form = songForm()
-    return render(response, 'song/downloadsong.html',{"form":form})
+        
+    return render(response, 'song/index.html',{"form":form})
+
+def executedownload(yurl):
+    try:
+        url = yurl
+        video = pafy.new(url)
+        bestaudio = video.getbestaudio()
+        bestaudio.download()
+        return True
+    except:
+        return False
+        
+    
